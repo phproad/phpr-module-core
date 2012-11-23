798437669f25998e4316368cb42f80a5fc094be5
Sam Georges <sam@frag.net.au>
2012-11-23 12:37:31 +1100

    Fixes format currency logic in the wrong place

--- helpers/core_locale.php
+++ helpers/core_locale.php
@@ -5,7 +5,10 @@ class Core_Locale
 
     public static function format_currency($value)
     {
-        return Phpr::$locale->get_currency($value);
+        if (class_exists('Payment_Config'))
+            return Payment_Config::format_currency($value);
+        else
+            return Phpr::$locale->get_currency($value);
     }
 
     public static function currency_symbol()
@@ -18,10 +21,7 @@ class Core_Locale
 
     public static function currency($value=null)
     {
-        if (class_exists('Payment_Config'))
-            return Payment_Config::format_currency($value);
-        else
-            return Phpr::$locale->get_string('phpr.currency', $value);
+        return Phpr::$locale->get_string('phpr.currency', $value);
     }
 
     public static function date($value=null)
