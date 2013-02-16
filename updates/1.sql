82aef786c98ed84ff03eb49a6cce9280367c2c09
Sam Georges <sam@daftspunk.com>
2013-02-16 13:25:56 +1100

    Move to Db_Structure methodology

--- updates/1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `core_settings` (
-  `id` int(11) NOT NULL auto_increment,
-  `record_code` varchar(50) default NULL,
-  `config_data` text,
-  PRIMARY KEY  (`id`),
-  KEY `record_code` (`record_code`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
